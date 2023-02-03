3e7a5f7b3a069c4d4b754d8b98172320b0b37ad2
boy-son <branden.wheat@yahoo.com>
2023-02-03 14:04:13 -0500

    added functions to view all employees, and by department/role

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mySQL = require('mysql2');
 const inquirer = require('inquirer');
 const { createConnection } = require('net');
+const { allowedNodeEnvironmentFlags } = require('process');
 require('console.table');
 const app = express();
 
@@ -16,6 +17,12 @@ const db = mysql.createConnection(
   },
   console.log(`Connected to the employees_db database.`)
 );
+
+db.connect(function(err) {
+if (err) throw err
+console.log("Connected as " + connection.threadID)
+Prompt1();   
+})
 
 function Prompt1() {
 inquirer
@@ -34,7 +41,7 @@ choices: [
 })
 
 .then(function({task}) {
-switch (task) {
+switch (task.choice) {
 case "All Employees":
 allEmployees();
 break;
@@ -43,12 +50,12 @@ case "Employees By Department":
 byDepartment();
 break;
 
-case "Add Employee":
-addEmployee();
+case "View All Roles":
+allRoles();
 break;
 
-case "Remove Employee":
-removeEmployee();
+case "Add Employee":
+addEmployee();
 break;
 
 case "Update Employee Role":
@@ -59,10 +66,13 @@ case "Add Role":
 addRole();
 break;
 
+case "Add Department":
+addDepartment();
+break;
+
 case "End":
 connection.end();
 break;
-
 
 }
 
@@ -70,5 +80,34 @@ break;
 
 }
 
+function allEmployees() {
+console.log('Viewing Employees');
+db.query(`SELECT employees.first_name, employees.last_name, role.title, role.salary, department.name, CONCAT(employees.first_name, ' ' , employees.last_name), AS Manager FROM employees INNER JOIN role on role.id = employee.role_id INNER JOIN department on department.id = role.department_id LEFT JOIN employee e on employee.manager_id = e.id;`,
+function(err, res) {
+if (err) throw err
+console.table(res)
+Prompt1();
+})
+}
 
+function byDepartment() {
+console.log('Viewing Employees By Department');
+db.query('SELECT employees.first_name, employees.last_name, department.name AS Department FROM employee JOIN role on employee.role_id = role.id JOIN department ON role.department_id = department.id ORDER BY employee.id',
+function(err, res) {
+if (err) throw err
+console.table(res)
+Prompt1();    
+}
+)    
+}
+
+function allRoles() {
+db.query('SELECT employees.first_name, employees.last_name, role.title AS Title FROM employees JOIN role on employee.role_id = role.id;',
+function (err,res) {
+if (err) throw err
+console.table(res)
+Prompt1();    
+}
+)    
+}
 
